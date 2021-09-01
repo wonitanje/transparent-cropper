9127e24ad4255be59a27ef95bedb608b47c73dc2
wonitanje <prod.juswee@gmail.com>
2021-09-02 01:59:22 +0700

    revised script output and fixed color detection

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 from PIL import Image
 from os import listdir, makedirs
 from os.path import exists
+from functools import reduce
 
 # Create result folder if not exist
 if not exists('cropped'):
   makedirs('cropped')
 
+skipped = []
+broken = []
+success = 0
+
 # For each image in folder
 for img_name in listdir():
-  format = img_name[img_name.rfind('.') + 1:]
-  if not format in ['png', 'jpg', 'jpeg']: continue
+  format = img_name.lower()[img_name.rfind('.') + 1:]
+  if not format in ['png', 'jpg', 'jpeg']:
+    skipped.append(img_name)
+    continue
   try:
     img = Image.open(f'{img_name}').convert('RGBA')
   except:
+    broken.append(img_name)
     continue
-  print(f'working with {img_name}')
+  print(f'Обрабатываю файл {img_name}')
+  success += 1
   img.load()
 
   # Constants of box
@@ -31,11 +40,7 @@ for img_name in listdir():
 
   # Pixel check for color
   def pixel_is_colorized(color):
-    try:
-      if color[3] == 0: return False
-    except:
-      if color == (255, 255, 255): return False
-    return True
+    return reduce(lambda sum, deep: sum + deep % 255, color, 0) != 0
 
   # Find top
   while not pixel_is_colorized(img.getpixel(tuple(top))):
@@ -67,11 +72,27 @@ for img_name in listdir():
 
   # Crop
   img_box = (left[0]-15, top[1]-15, right[0]+15, bottom[1]+15)
-  print(f'{img.size=}\ncropping by {img_box}\n')
   cropped_size = (img_box[2] - img_box[0], img_box[3] - img_box[1])
 
   cropped = Image.new('RGBA', cropped_size, (255, 255, 255))
+  print(f'Перевожу\nИз: {img.size}\nВ: {cropped.size}\nКоординаты обрезки: {img_box}\n\n')
 
   img_pos = tuple(map(lambda x: -x, img_box[:2]))
   cropped.paste(img, img_pos, img)
   cropped.convert('RGB').save(f'cropped/{strip_format(img_name)}.jpg')
+
+print('Программа завершила работу')
+print(f'{success} изображений отредактированно')
+print(f'{len(broken)} изображений не удалось отредактировать')
+print(f'{len(skipped)} файлов прощенно')
+
+if '1' in input("\nВведите '1', чтобы показать неотредактированные и пропущенные файлы: "):
+  print('\nНеотредактированные изображения:')
+  for (idx, item) in enumerate(broken):
+    print(f'{idx}: {item}')
+
+  print('\nПропущенные файлы:')
+  for (idx, item) in enumerate(skipped):
+    print(f'{idx}: {item}')
+
+input("\nНажмите 'Enter', чтобы закрыть программу ")
